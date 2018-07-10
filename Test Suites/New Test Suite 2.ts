fbdac7eb6158e055bdd5e0dc7ab5b585b8661876
MSTEMP194 <MSTEMP194@MSTSYS0194>
2018-07-10 21:09:23 +0530

    commit

--- Test Suites/New Test Suite 2.ts
+++ Test Suites/New Test Suite 2.ts
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>New Test Suite</name>
+   <name>New Test Suite 2</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-07-10T21:00:26</lastRun>
+   <lastRun>2018-07-10T21:06:30</lastRun>
    <mailRecipient>sindhube19.data@gmail.com;</mailRecipient>
    <numberOfRerun>2</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>3643ddef-a0dc-4e2a-9165-04744878017f</testSuiteGuid>
+   <testSuiteGuid>f4f43b8d-79bb-4c37-af35-bf8f6a8b2256</testSuiteGuid>
    <testCaseLink>
-      <guid>bfdb7ab8-f2d6-4ca4-9cc7-c0b4bbb25f1e</guid>
+      <guid>fcccbd10-184b-4593-9226-d479edd62dc6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/testcase1</testCaseId>
