e18aa1d7be6a85b295b01cc598d42a56f577a3eb
MSTEMP194 <MSTEMP194@MSTSYS0194>
2018-07-11 10:50:35 +0530

    wenesday commit

---
+++ Test Suites/New Test Suite 3.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite 3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-07-10T21:11:19</lastRun>
+   <mailRecipient>sindhuja.e@mstsolutions.com;</mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0642270f-6a30-41f9-a036-be3154c4c7d9</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc645e28-b2e1-4dda-8d63-4fce2ec30648</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
